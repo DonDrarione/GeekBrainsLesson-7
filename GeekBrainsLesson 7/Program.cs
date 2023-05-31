5c917c1501b664c0b32cde6ca5c0afd467822a41
sunny <sunny@DESKTOP-UJ1BUSP>
2023-05-31 21:29:48 +0300

    Добавьте файлы проекта.

---
+++ GeekBrainsLesson 7/Program.cs
@@ -0,0 +1,13 @@
+﻿var m = Convert.ToInt32(Console.ReadLine()); //объявляем переменную
+var n = Convert.ToInt32(Console.ReadLine());
+var m_n = new double[m, n]; //вводим двумерный массив=матрица
+var random = new Random(); //генератор рандомных чисел
+for (int i = 0; i < m; i++) //for условие, i индекс по вертикали, j по горизонтали и мы перебираем построчно (++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        m_n[i, j] = Math.Round(random.NextDouble() + random.Next(-10, 10), 1); //объявляем значение для каждого элемента и округляем его до 1 числа после запятой
+        Console.Write(m_n[i, j] + "\t"); //выводим каждое значение по строчкам
+    }
+    Console.WriteLine();//переход на новую строку
+}
